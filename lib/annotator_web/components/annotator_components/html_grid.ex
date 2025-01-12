ed74e8fe299b6fc20f7264a9958299f69d4918a4
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2025-01-12 01:24:14 -0500

    nicer html export and separate css for it

--- lib/annotator_web/components/annotator_components/html_grid.ex
+++ lib/annotator_web/components/annotator_components/html_grid.ex
@@ -40,43 +40,46 @@ defmodule AnnotatorWeb.AnnotatorComponents.HtmlGrid do
     role="grid"
     tabindex="0"
     id={"annotated-content-#{@collection_id}"}
-    phx-hook="GridNav" >
-      <div role="rowgroup" class="subgrid-holder">
+    phx-hook="GridNav">
+
+      <div role="rowgroup" class="subgrid-holder column-labels">
         <div role="row" class="subgrid-holder row-container">
           <div :for={col <- @col} role="columnheader" class="columnheader">
             <%= col[:label] %>
           </div>
         </div>
       </div>
+
       <div role="rowgroup" class="subgrid-holder">
         <%= for {{chunk, lines}, row_index} <- Enum.with_index(@chunk_groups) do %><div
             role="row"
             class="subgrid-holder row-container"
             style={AnnotatorComponents.rowspanstyle(lines)}
-            ><%= for {col, col_index} <- Enum.with_index(@col) do %><div
-                role="gridcell"
-                tabindex="-1"
-                id={"cell-#{row_index}-#{col_index}"}
-                data-col-index={col_index}
-                data-row-index={row_index}
-                aria-label={AnnotatorComponents.get_aria_label(col, lines, chunk)}
-                class={[
-                  "grid-cell",
-                  col[:name],
-                  col_index != length(@col) - 1 && "border-r"
-                ]}
-                style={AnnotatorComponents.rowspanstyle(lines)}
-                >
-                <.static_cell_content
+            >
+          <%= for {col, col_index} <- Enum.with_index(@col) do %>
+            <div
+              role="gridcell"
+              tabindex="-1"
+              id={"cell-#{row_index}-#{col_index}"}
+              data-col-index={col_index}
+              data-row-index={row_index}
+              aria-label={AnnotatorComponents.get_aria_label(col, lines, chunk)}
+              class={[
+                "grid-cell",
+                col[:name]
+              ]}
+              style={AnnotatorComponents.rowspanstyle(lines)}
+              ><.static_cell_content
                   col={col}
+                  col_name={col[:name]}
                   row_index={row_index}
                   col_index={col_index}
                   chunk={chunk}
                   row_lines={lines}
                   lang={@lang}
                 />
-              </div>
-            <% end %>
+            </div>
+          <% end %>
           </div>
         <% end %>
       </div>
@@ -85,44 +88,40 @@ defmodule AnnotatorWeb.AnnotatorComponents.HtmlGrid do
   end
 
   attr :col, :any, required: true
+  attr :col_name, :string, required: true
   attr :chunk, :any
   attr :row_lines, :list
   attr :row_index, :string
   attr :col_index, :string
   attr :lang, :string, default: ""
 
-  def static_cell_content(assigns) do
+  def static_cell_content(%{col_name: "content"} = assigns) do
     ~H"""
-    <%= case @col[:name] do %>
-      <% "line-num" -> %>
-        <div class="line-numbers flex flex-col">
+              <%= for line <- @row_lines do %>
+                <div class="content-line"><pre class="whitespace-pre-wrap add-number" data-number={line.line_number}><code class={"language-#{@lang}"} ><%= raw line.content %></code></pre></div><% end %>
+    """
+  end
+
+  def static_cell_content(%{col_name: "note"} = assigns) do
+    ~H"""
+          <%= Phoenix.HTML.raw Earmark.as_html!(@chunk.note, breaks: true) || "No note" %>
+    """
+
+  end
+
+  def static_cell_content(%{col_name: "line_num"} = assigns) do
+    ~H"""
           <%= for line <- Enum.sort(@row_lines, &(&1.line_number <= &2.line_number)) do %>
             <pre><code><%= line.line_number %></code></pre>
           <% end %>
-        </div>
-      <% "content" -> %>
-        <div
-            class="content ml-2 flex flex-col"
-            role="presentation"
-            phx-click={JS.focus(to: "#cell-#{@row_index}-#{@col_index}")}
-            phx-value-row={@row_index}
-            phx-value-col={@col_index}
-          >
-          <%= for line <- @row_lines do %>
-              <div class="content-line"><pre class="whitespace-pre-wrap add-number" data-number={line.line_number}><code class={"language-#{@lang}"} ><%= raw line.content %></code></pre></div>
-          <% end %>
-        </div>
-      <% "note" -> %>
-        <div class="py-2 px-4">
-          <%= Phoenix.HTML.raw Earmark.as_html!(@chunk.note, breaks: true) || "No note" %>
-        </div>
-      <% _ -> %>
-        <%= if @col do %>
-          <%= inspect(@col) %>
-        <% else %>
-          Even col isn't there
-        <% end %>
-    <% end %>
+    """
+  end
+
+  def static_cell_content(assigns) do
+    ~H"""
+
+        Invalid col name
+
     """
   end
 
